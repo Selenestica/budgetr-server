389a2b6e329d8b0cf18c478530f65f0aaa5967fb
JB Wilson <53917177+Selenestica@users.noreply.github.com>
2020-04-03 00:36:15 -0500

    added code to send an email when a user wants to register. unsure if it works and needs some major clean-up

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const models = require("../models");
 global.bcrypt = require("bcrypt");
 global.SALT_ROUNDS = 10;
+const nodemailer = require("nodemailer");
+const jwt = require("jsonwebtoken");
+const argon2 = require("argon2");
+const crypto = require("crypto");
 
 //====================REGISTER====================//
 router.post("/register", async (req, res) => {
@@ -10,9 +14,7 @@ router.post("/register", async (req, res) => {
     last = req.body.last,
     email = req.body.email,
     phone_number = req.body.phone_number,
-    password = req.body.password,
-    display_name = req.body.display_name,
-    profile_picture_url = req.body.profile_picture_url;
+    password = req.body.password;
 
   let persistedUser = await models.Users.findOne({
     where: {
@@ -30,16 +32,22 @@ router.post("/register", async (req, res) => {
         console.log(error);
         res.send("Error creating user ¯_(ツ)_/¯");
       } else {
-        let user = models.Users.build({
+        // creating an email verification token
+        const emailVerificationToken = crypto.randomBytes(20).toString("hex");
+
+        // the token expires in one day
+        const emailVerificationExpires = new Data(Date.now() + 86400000);
+
+        const user = models.Users.build({
           first: first,
           last: last,
           email: email,
           phone_number: phone_number,
           password: hash,
-          display_name: display_name,
-          profile_picture_url: profile_picture_url
+          emailVerificationToken: emailVerificationToken,
+          emailVerificationExpires: emailVerificationExpires
         });
-        let savedUser = await user.save();
+        const savedUser = await user.save();
 
         await console.log(savedUser);
         if (savedUser !== null) {
@@ -49,6 +57,48 @@ router.post("/register", async (req, res) => {
             "There is already a user who has registered with that information ¯_(ツ)_/¯"
           );
         }
+
+        // sending email to verify provided email address
+        console.log("Sending email to verify provided email address...");
+
+        const transporter = nodemailer.createTransport({
+          service: config.email.service,
+          secure: false,
+          auth: {
+            user: config.email.user, // ethereal user
+            pass: config.email.password // ethereal password
+          }
+        });
+
+        const emailVerificationMessage = `
+          <div>
+            <p>Welcome to Budgetr!</p>
+            <p>Please click the link below to continue your registration. Thanks!</p>
+            <a href = http://localhost:3000/verifyEmail/${email}/${emailVerificationToken}>Verify Email</a>
+          </div>
+        `;
+
+        const messageSent = await transporter.sendMail({
+          from: "joebenwilsonmusic@gmail.com",
+          to: email,
+          subject: "Welcome to Budgetr!",
+          html: emailVerificationMessage
+        });
+
+        if (!messageSent) {
+          console.log(
+            "There was an error sending the email. Our cyborgs have failed you :("
+          );
+        }
+
+        const token = jwt.sign({
+          ...savedUser
+        });
+
+        return res.status(200).json({
+          savedUser,
+          token
+        });
       }
     });
   } else {
@@ -58,12 +108,14 @@ router.post("/register", async (req, res) => {
   }
 });
 
+// see all registered users. delete this one XD
 router.get("/view-users", (req, res) => {
   models.Users.findAll().then(users => {
     res.json({ RegisteredUsers: users });
   });
 });
 
+// remove a user
 router.post("/delete-user/:id", (req, res) => {
   models.Users.destroy({
     where: {
